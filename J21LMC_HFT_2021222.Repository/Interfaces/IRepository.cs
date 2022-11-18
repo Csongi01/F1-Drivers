3f00c4d0b0bce0886aa33d08e306d17f4f9a5601
Csongi01 <116305246+Csongi01@users.noreply.github.com>
2022-11-18 15:27:17 +0100

    Repository interfaces added

---
+++ J21LMC_HFT_2021222.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J21LMC_HFT_2021222.Repository
+{
+    public interface IRepository<T>
+
+    {
+        IQueryable<T> ReadAll();
+        T Read(string id); // race_code miatt string
+        void Create(T item);
+        void Update(T item);
+        void Delete(string id); // race_code miatt string
+    }
+}
---
+++ J21LMC_HFT_2021222.Repository/Interfaces/IResultRepository.cs
@@ -0,0 +1,13 @@
+﻿using J21LMC_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J21LMC_HFT_2021222.Repository
+{
+    public interface IResultRepository: IRepository<Result>
+    {
+    }
+}
