2ec7d8d707e306614c14ce3cab65764454d11e61
Csongi01 <116305246+Csongi01@users.noreply.github.com>
2022-11-18 15:34:42 +0100

    endpoint properties & startup added

--- J21LMC_HFT_2021222.Endpoint/Startup.cs
+++ J21LMC_HFT_2021222.Endpoint/Startup.cs
@@ -1,8 +1,16 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using J21LMC_HFT_2021222.Logic;
+using J21LMC_HFT_2021222.Models;
+using J21LMC_HFT_2021222.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,28 +20,60 @@ namespace J21LMC_HFT_2021222.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+      
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<F1DbContext>();
+
+            services.AddTransient<IRepository<Pilot>, PilotRepository>();
+            services.AddTransient<IRepository<Race>, RaceRepository>();
+            services.AddTransient<IRepository<Result>, ResultRepository>();
+            services.AddTransient<IRepository<Team>, TeamRepository>();
+
+            services.AddTransient<IPilotLogic, PilotLogic>();
+            services.AddTransient<IRaceLogic, RaceLogic>();
+            services.AddTransient<IResultLogic, ResultLogic>();
+            services.AddTransient<ITeamLogic, TeamLogic>();
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "J21LMC_HFT_2021222.Endpoint", Version = "v1" });
+            });
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "J21LMC_HFT_2021222.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
