8db1c484d1502b17860e148079e20f45641b6569
Csongi01 <116305246+Csongi01@users.noreply.github.com>
2022-11-18 15:24:35 +0100

    team & entity modell added

---
+++ J21LMC_HFT_2021222.Models/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J21LMC_HFT_2021222.Models
+{
+    public abstract class Entity
+    {
+        [Key]        
+        public virtual int Id { get; set; }
+    }
+}
---
+++ J21LMC_HFT_2021222.Models/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace J21LMC_HFT_2021222.Models
+{
+    [Table("teames")]
+    public class Team 
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int team_id { get; set; }
+        [Required]
+        public string team_name { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Pilot> Pilots { get; set; }
+        public Team()
+        {
+            this.Pilots = new HashSet<Pilot>();
+        }
+
+    }
+}
